1638f8ccc1c67e173b58f3d52a48ddc84521029b
Aaron <ajblock@gmail.com>
2023-02-04 16:29:00 -0500

    Component icons added

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,12 +1,16 @@
+import React from 'react';
 import { NavLink } from 'react-router-dom';
+import dtf from '../../assets/dtf.svg';
+import gauge from '../../assets/gauge.svg';
+import angle from '../../assets/angefind.svg';
 
 const NavBar: React.FC = () => {
     return (
         <div className="nav-bar">
             <ul>
-                <li><NavLink to="/dec-to-frac">Dec To Frac</NavLink></li>
-                <li><NavLink to="/gauge-meter">Gauge Meter</NavLink></li>
-                <li><NavLink to="/angle-finder">AngleFinder</NavLink></li>
+                <li><NavLink to="/dec-to-frac"><img src={dtf} /></NavLink></li>
+                <li><NavLink to="/gauge-meter"><img src={gauge} /></NavLink></li>
+                <li><NavLink to="/angle-finder"><img src={angle} /></NavLink></li>
             </ul>
         </div>
     );
